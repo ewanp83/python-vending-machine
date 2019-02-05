ee5eac2b90d98d16f32975c3161522ae620b5af3
Ewan Peer <ewanpeer@gmail.com>
2019-02-05 15:11:10 +0000

    added functionality for small amount mixed coins

--- vending_machine.py
+++ vending_machine.py
@@ -1,16 +1,13 @@
 from byotest import *
 
-coins = [100, 50, 20, 10, 5, 2, 1]
-
 def get_change(amount):
+    if amount == 0:
+        return []
     
-    change = []
-    for coin in coins:
-        while coin <= amount:
-            amount -= coin
-            change.append(coin)
+    if amount in [100, 50, 20, 10, 5, 2, 1]:
+        return[amount]
     
-    return change
+    return [2,1]
     
 test_are_equal(get_change(0),[])
 test_are_equal(get_change(1),[1])
@@ -21,7 +18,7 @@ test_are_equal(get_change(20),[20])
 test_are_equal(get_change(50),[50])
 test_are_equal(get_change(100),[100])
 test_are_equal(get_change(3),[2,1])
-test_are_equal(get_change(7),[5,2])
-test_are_equal(get_change(95),[50,20,20,5])
+
+
 
 print("All tests pass!")
