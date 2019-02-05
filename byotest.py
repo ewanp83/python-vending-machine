64127aa4bb22966a48d609280fa5742a77196885
Ewan Peer <ewanpeer@gmail.com>
2019-02-05 15:09:06 +0000

    basic function

---
+++ byotest.py
@@ -0,0 +1,17 @@
+
+def test_are_equal(actual, expected):
+    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+
+
+def test_not_equal(a, b):
+    assert a != b, "Did not expect {0} but got {1}".format(a, b)
+
+
+def test_is_in(collection, item):
+    assert item in collection, "{0} does not contain {1}".format(collection, item)
+
+def test_not_in(collection, item):
+    assert item not in collection, "{0} is in {1}".format(item, collection) 
+    
+def test_between(lower_limit , upper_limit, actual):
+    assert lower_limit >= actual >= upper_limit, "{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
